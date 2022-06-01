dc129d18d59ed201bfdfb291b8d5a713375e215d
Kevin Zhong <kvnzhong@gmail.com>
2022-06-01 00:11:16 -0700

    Implemented authorization.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/users/:id", controller: "users", action: "show"
+  post "/users", controller: "users", action: "create"
+  post "/sessions", controller: "sessions", action: "create"
 end
